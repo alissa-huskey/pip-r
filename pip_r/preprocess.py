97688d78a1d31ce7bd87af00837548496da2f894
Alissa Huskey <alissa.huskey@gmail.com>
2022-03-17 06:59:23 -0600

    add Processor.process() to switch out env vars

--- pip_r/preprocess.py
+++ pip_r/preprocess.py
@@ -1,3 +1,6 @@
+from os import environ
+from collections import defaultdict
+
 from parsimonious.nodes import NodeVisitor
 from parsimonious.grammar import Grammar
 
@@ -21,26 +24,67 @@ var_close           = "}"
 backslash           = "\57"
 """
 
+grammar = Grammar(spec)
+
+class EnvVar():
+    """A class for the environment variables found when parsing."""
+    def __init__(self, node):
+        self.node = node
+        self.name = node.text
+
+    def __repr__(self):
+        return f"EnvVar(name={self.name!r})"
+
+    @property
+    def value(self):
+        """Return the value in the current environment."""
+        return environ.get(self.name, "")
+
+    @property
+    def placeholder(self):
+        """Return the placeholder text that will be replace."""
+        return "${%s}" % self.name
+
+    def replace(self, text):
+        """Return text with placedholders replaced with values."""
+        return text.replace(self.placeholder, self.value)
+
 class Preprocessor(NodeVisitor):
+    """A parsing class for preprocessing the requirements.txt file. (Just
+    switching out environment variables.)"""
 
     def __init__(self, content):
         super().__init__()
         self.grammar = Grammar(spec)
-        self.content = content
-        self.result = {}
+        self.content = self.original = content
+        self.vars = defaultdict(lambda: None)
+        self.parsed = False
 
     def visit_root(self, node, visited_children):
-        return self.result
+        return self.vars
 
     def visit_var_name(self, node, visited_children):
+        """Create an EnvVar object for each unique var_name found."""
         if not node.text:
             return
 
-        self.result[node.text] = node
+        self.vars[node.text] = self.vars[node.text] or EnvVar(node)
 
     def generic_visit(self, node, visited_children):
         """ The generic visit method. """
         return node or visited_children
 
     def parse(self):
+        """Parse the saved content and turn on self.parsed"""
+        self.parsed = True
         return super().parse(self.content)
+
+    def process(self):
+        """Replace all environment variables in self.content with their
+           respective values."""
+        if not self.parsed:
+            raise Exception("You have to parse first!")
+
+        for var in self.vars.values():
+            self.content = var.replace(self.content)
+        return self.content
--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -1,10 +1,12 @@
+from os import environ
 import pytest
 
-from pip_r.preprocess import Preprocessor
+from pip_r.preprocess import Preprocessor, EnvVar, grammar
 from pip_r.grammar import groups
 
+from tests.stubs import Stub
+
 @pytest.mark.parametrize("text, count, names", [
-    #  (r"pip @ ${HOME}/downloads/numpy-\${VERSION}-cp34-none-win32.whl", 1, ["HOME"]),
     ("pip @ ./downloads/numpy-1.9.2-cp34-none-win32.whl", 0, []),
     ("pip @ ${HOME}/downloads/numpy-1.9.2-cp34-none-win32.whl", 1, ["HOME"]),
     ("pip @ ${HOME}/downloads/numpy-${VERSION}-cp34-none-win32.whl", 2, ["HOME", "VERSION"]),
@@ -31,4 +33,39 @@ def test_parse(text, count, names):
 
     assert len(doc) == count
     for var in names:
-        assert doc.get(var)
+        assert doc[var]
+
+def test_env_var():
+    environ["STAR"] = "*"
+    node = Stub(text="STAR", start=8, end=12)
+    var = EnvVar(node)
+
+    assert var.placeholder == "${STAR}"
+    assert var.name == "STAR"
+    assert var.value == "*"
+    assert var.replace("twinkle twinkle little ${STAR}") == "twinkle twinkle little *"
+
+@pytest.mark.parametrize("data", [
+    Stub(
+        env_vars=dict(COLOR="gray", R="100", G="100", B="100"),
+        input="The color ${COLOR} is (${R}, ${G}, ${B}).",
+        expected="The color gray is (100, 100, 100).",
+    ),
+    Stub(
+        env_vars=dict(HOME="/Users/dev"),
+        input="${HOME} sweet ${HOME}.",
+        expected="/Users/dev sweet /Users/dev.",
+    ),
+    Stub(
+        env_vars={},
+        input="Oh hai there.",
+        expected="Oh hai there.",
+    ),
+])
+def test_replace(data):
+    environ.update(data.env_vars)
+    text = data.input
+
+    parser = Preprocessor(text)
+    doc = parser.parse()
+    assert parser.process() == data.expected
